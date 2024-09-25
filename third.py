f83387aa7bfc3fa19fee1d1df2acfa54edb96c07
unknown <Abass>
2024-09-25 16:26:40 +0100

    i have added some texts

---
+++ third.py
@@ -0,0 +1,7 @@
+def sobur():
+    print("Sobur is at my back")
+    print("He is a black boy")
+    print("he is a short boy")
+    print("he is a very bad retailer, he does not satisfy his consumner wants")
+sobur()    
+
